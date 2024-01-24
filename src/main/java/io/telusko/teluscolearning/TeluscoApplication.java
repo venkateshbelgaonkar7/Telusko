ec43dfbc4796bb26ce1a4c815807399e1084e27c
venkateshbelgaonkar7 <Baloney@1234567>
2024-01-24 19:34:13 +0530

    Added Hibernate examples from telusko learning

---
+++ src/main/java/io/telusko/teluscolearning/TeluscoApplication.java
@@ -0,0 +1,13 @@
+package io.telusko.teluscolearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeluscoApplication.class, args);
+    }
+
+}
